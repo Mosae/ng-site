d3fd997c5d40786c7bd3e0f77076f77ae45ee3a0
Mosae-Litsoane <mosaelitsoane@gmail.com>
2021-02-05 20:46:14 -0600

    Created routes - minor bugs to fix

---
+++ src/app/sections/section-sales/section-sales.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-section-sales',
+  templateUrl: './section-sales.component.html',
+  styleUrls: ['./section-sales.component.scss'],
+})
+export class SectionSalesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
